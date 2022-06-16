c5a4424ea512e542f5dc8a02ff22bb2d13a5af30
Darlan Araujo <darlanparaujo@outlook.com>
2022-06-16 00:37:50 -0300

    Atualização #12
    Concluído animação com scroll. os itens da tela se movem de acordo com o movimento da tela.

--- js/script.js
+++ js/script.js
@@ -21,6 +21,29 @@ initMenuFixo();
 // ===================================
 // ANIMAÇÃO SEÇÃO QUEM SOMOS
 // ===================================
+function initAnimaScroll() {
+    const itensMove = document.querySelectorAll('.js-move');
+    itensMove[0].classList.add('active');
+    
+    window.addEventListener('scroll', (event) => {
+        itensMove.forEach((item, index) => {
+    
+            const distancia = item.getBoundingClientRect().top;
+            const mediaTela = window.innerHeight * 0.9;
+    
+            if(index != 0) {
+                if(distancia < mediaTela) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active')
+                }
+            }
+        });
+    });
+}
+
+initAnimaScroll();
+
 
 
 
