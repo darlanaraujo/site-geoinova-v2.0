5803cce2e77eb270e263a63b0feb39cae9ecccff
Darlan Araujo <darlanparaujo@outlook.com>
2022-06-17 09:47:17 -0300

    Atualização #14
    menuScroll - Ao rolar o mouse pela tela o menu fixo vai mudando a classe active de acordo com a janela em foco atual.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // ===================================
 // MENU FIXO
 // ===================================
-
 function initMenuFixo() {
     window.addEventListener('scroll', () => {
         const menuFixo = document.querySelector('.js-header-top-fixo');
@@ -15,7 +14,6 @@ function initMenuFixo() {
         }
     });
 }
-
 initMenuFixo();
 
 // ===================================
@@ -41,34 +39,66 @@ function initAnimaScroll() {
         });
     });
 }
-
 initAnimaScroll();
 
 
 // ===================================
-// SCROLL SUAVE
+// ATIVAÇÃO DO MENU COM CLICK
 // ===================================
-const menu = document.querySelectorAll('.js-menu');
+function initMenuClick(){
+    const menu = document.querySelectorAll('.js-menu');
 
-menu.forEach((item) => {
-    item.addEventListener('click', (event) => {
-        // Uso o event para saber o nome do botão clicado
-        const id = event.target.hash; // Ex: #faq
+    menu.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            // Uso o event para saber o nome do botão clicado
+            const id = event.target.hash; // Ex: #faq
 
-        // Crio uma NodeList usando o nome clicado como seletor
-        const target = document.querySelectorAll(`[href^="${id}"]`);
+            // Crio uma NodeList usando o nome clicado como seletor
+            const target = document.querySelectorAll(`[href^="${id}"]`);
 
-        // Removo todos as classes actives de todos os menus
-        menu.forEach((item) => {
-            item.classList.remove('active')
+            // Removo todos as classes actives de todos os menus
+            menu.forEach((item) => {
+                item.classList.remove('active')
+            });
+
+            // Uso apenas a NodeList criada para ativar a classe
+            target.forEach((item) => {
+                item.classList.add('active');
+            });
+
         });
-
-        // Uso apenas a NodeList criada para ativar a classe
-        target.forEach((item) => {
-            item.classList.add('active');
-        });
-
     });
-});
+}
+initMenuClick();
 
+// ===================================
+// ATIVAÇÃO DO MENU COM SCROLL
+// ===================================
+function initMenuScroll() {
+    const menuFixo = document.querySelectorAll('.js-header-top-fixo .js-menu');
+    const menu = document.querySelectorAll('.js-menu');
+    const section = document.querySelectorAll('.js-section');
+    
+    section.forEach((item, index) => {
+    
+        window.addEventListener('scroll', () => {
+            const itemTop = item.getBoundingClientRect().top.toFixed(2);
+            const itemBottom = item.getBoundingClientRect().bottom.toFixed(2);
+            const mediaTela = window.innerHeight * 0.3;
+    
+            
+    
+            if(itemTop <= mediaTela && itemBottom > mediaTela) {
+                menu.forEach((item) => {
+                    item.classList.remove('active');
+                });
+    
+                menuFixo[index].classList.add('active');
+            } else {
+                menuFixo[index].classList.remove('active');
+            }
+        });
+    });
+}
+initMenuScroll();
 
